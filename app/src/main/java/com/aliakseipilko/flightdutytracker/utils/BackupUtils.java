1fa1ee93d4b26db6d80523013713633e28e92505
Aliaksei Pilko <aliakseipilko1@gmail.com>
2017-01-28 12:22:00 +0000

    Completed Backup Utils

--- app/src/main/java/com/aliakseipilko/flightdutytracker/utils/BackupUtils.java
+++ app/src/main/java/com/aliakseipilko/flightdutytracker/utils/BackupUtils.java
@@ -11,33 +11,73 @@
 package com.aliakseipilko.flightdutytracker.utils;
 
 
+import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.google.gson.stream.JsonWriter;
 
 import com.aliakseipilko.flightdutytracker.realm.model.Flight;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.text.DateFormat;
 
+import io.realm.Realm;
 import io.realm.RealmResults;
 
 public class BackupUtils {
 
-    public static File serialiseFlightsRealmToFile(File destFile, Gson gson, RealmResults<Flight> data) throws IOException {
+    public static File serialiseFlightsRealmToFile(File destFile, RealmResults<Flight> data) throws IOException {
         destFile.setWritable(true);
         if (!destFile.canWrite()) {
             return null;
         }
 
-        JsonWriter writer = gson.newJsonWriter(new FileWriter(destFile));
-        FlightSerialiser flightSerialiser = new FlightSerialiser();
+        GsonBuilder gsonBuilder = new GsonBuilder();
+        Gson gson = gsonBuilder
+                .registerTypeAdapter(Flight.class, new FlightSerialiser())
+                .serializeNulls()
+                .setFieldNamingPolicy(FieldNamingPolicy.IDENTITY)
+                .setDateFormat(DateFormat.FULL, DateFormat.FULL)
+                .create();
 
-        writer.beginObject();
-        gson.toJson(data, Flight.class, writer);
-        writer.endObject();
+        JsonWriter writer = gson.newJsonWriter(new FileWriter(destFile));
+
+        for (Flight f : data) {
+            writer.beginObject();
+            gson.toJson(f, Flight.class, writer);
+            writer.endObject();
+        }
 
         return destFile;
+    }
+
+    public static void deserialiseFlightsFileToRealm(Realm realm, final File srcFile) {
+
+        realm.executeTransactionAsync(new Realm.Transaction() {
+            @Override
+            public void execute(Realm realm) {
+                try {
+                    InputStream is = new FileInputStream(srcFile);
+                    realm.createAllFromJson(Flight.class, is);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Realm.Transaction.OnSuccess() {
+            @Override
+            public void onSuccess() {
+                //TODO Handle
+            }
+        }, new Realm.Transaction.OnError() {
+            @Override
+            public void onError(Throwable error) {
+                //TODO Handle
+            }
+        });
     }
 
 }
